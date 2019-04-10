3a1af03a0334c8e1d6fa656fd56bf60fd2045f17
João Filipe Matos Silva <joao@kspace.pt>
2019-04-10 22:11:02 +0100

    Explicitly set AddEnvironmentVariables

--- MewtwoCounter/Program.cs
+++ MewtwoCounter/Program.cs
@@ -19,6 +19,10 @@ namespace MewtwoCounter
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .UseStartup<Startup>();
     }
 }
